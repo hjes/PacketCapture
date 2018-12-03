157c33783af4b271e3c479f418991f7710aba6c0
hongqianhui <hong19961218@qq.com>
2018-12-03 14:33:56 +0800

    'update'

--- src/main/java/demo/controller/HomeController.java
+++ src/main/java/demo/controller/HomeController.java
@@ -16,17 +16,10 @@ import javax.annotation.PostConstruct;
 @ViewController(value = "/views/home.fxml")
 public class HomeController {
 
-    @FXML private JFXButton homeBtn1;
     @FXML private BorderPane root;
 
     @PostConstruct
     private void init(){
-        homeBtn1.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                System.out.println("you click btn1");
-                NotificationUtils.notifySuccess("oh kaqima",root);
-            }
-        });
+
     }
 }
